b3baf26cd5e30cc71724dbee43943f71f1fb3fce
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-09-08 19:56:41 +0530

    add minor changes

--- webCrawler.cpp
+++ webCrawler.cpp
@@ -5,12 +5,17 @@
 int main(int argc,char *argv[])
 {
     printf("PROGRAMME NAME IS %s \n",argv[0]);
-    if(argc <2)
+    if(argc >2)
     {
-        printf("The arguments supplied are %s %s %s",argv[1]);
+	for(int i = 0 ;i<argc;i++)
+	{
+        	printf("\nThe arguments supplied are %s\n",argv[i]);
+	}
     }
-    else{
-        printf("TOO MANY ARGUMENTS");
+	
+    else 
+    {
+		printf("THE PROGRAMME CONTAINS NO ARGUMENTS");
     }
     return 0;
 }
