971820e1dc64ece19dc49bee96b766ccab3df712
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-09-08 19:40:04 +0530

    add minor change in cli

--- webCrawler.cpp
+++ webCrawler.cpp
@@ -2,4 +2,15 @@
 #include<stdio.h>
 
 
-int Crawler(char *argv,int argc)
+int main(int argc,char *argv[])
+{
+    printf("PROGRAMME NAME IS %s \n",argv[0]);
+    if(argc <2)
+    {
+        printf("The arguments supplied are %s %s %s",argv[1]);
+    }
+    else{
+        printf("TOO MANY ARGUMENTS");
+    }
+    return 0;
+}
