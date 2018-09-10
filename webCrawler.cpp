71bf808e480c23bf080737d3953beb35c71f2f60
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-09-10 11:40:03 +0530

    add extract url func

--- webCrawler.cpp
+++ webCrawler.cpp
@@ -1,11 +1,59 @@
 #include<iostream>
 #include<stdio.h>
+#include<stdlib.h>
 #include<fstream>
 #define HASH_SIZE 100
 #define URL_LENGTH 1000
 #define SEED_URL "http://www.chitkara.edu.in"
 #define MAX_URL_PER_PAGE 1000
 #define INTERVAL_TIME 10
+/* FUNCTION TO INSERT INTO FILES */
+using namespace std;
+void insertIntoFILE()
+{
+    FILE *inputFile;
+    inputFile =fopen("temp.txt","w");
+
+    if (inputFile==NULL)
+    {
+        fprintf(stderr," ERROR OPENING FILE ");
+    }
+    else
+    {
+        fprintf(inputFile,"FILE OPENED %s",system("curl https://www.chitkara.edu.in > temp.txt"));
+    }
+}
+
+/* ********************************************************* */
+
+/* FUNCTION TO EXTRACT URLS FROM THE FILE */
+void extractURLS()
+{
+    FILE *extractData;
+    extractData = fopen("temp.txt","r");
+    char ch;
+
+    if(extractData == NULL)
+    {
+        printf("INSERT A HTML PAGE");
+        exit(0);
+    }
+    else
+    {
+        ch = fgetc(extractData);
+
+        while(ch!= EOF)
+        {
+            if(ch!='<' && ch!='>')
+            {
+                cout<<ch<<endl;
+            }
+            ch = fgetc(extractData);
+        }
+    }
+
+}
+/* END OF EXTRACT FUNCTION */
 int main(int argc,char *argv[]) // command line arguments
 {
     printf("PROGRAMME NAME IS %s \n",argv[0]);
@@ -22,17 +70,8 @@ int main(int argc,char *argv[]) // command line arguments
         printf("THE PROGRAMME CONTAINS NO ARGUMENTS");
     }
 
-    FILE *inputFile;
-    inputFile =fopen("temp.txt","w");
 
-    if (inputFile==NULL)
-    {
-        fprintf(stderr," ERROR OPENING FILE ");
-    }
-    else
-    {
-        fprintf(inputFile,"FILE OPENED %s",system("curl https://www.chitkara.edu.in > temp.txt"));
-    }
-
+    insertIntoFILE();
+    extractURLS();
     return 0;
 }
